4be3433054a846af88c3e50de9bc2a463d4314a5
Rafael García <rafalbergh@gmail.com>
2025-04-27 10:56:13 -0600

    Initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Header.css'
+
+function Header () {
+
+
+    return(
+        <>
+        <div className="header">
+            <h1>Mis tareas</h1>
+            <button>Agregar tarea</button>
+        </div>
+        </>
+    )
+}
+
+export default Header
