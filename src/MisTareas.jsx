835f8bfcddfd85599f79647b4332bf4eb989ac75
Rafael García <rafalbergh@gmail.com>
2025-05-10 19:30:05 -0600

    Corrección de error en resumen de tareas

--- src/MisTareas.jsx
+++ src/MisTareas.jsx
@@ -7,17 +7,13 @@ import ListaTareas from './ListaTareas';
 function MisTareas() {
   const [verForm, setVerForm] = useState(false);
   const [listaTareas, setTareas] = useState([]);
-  const [pendientes, setPendientes] = useState(0);
-  const [completadas, setCompletadas] = useState(0);
   const [resumen, setResumen] = useState("");
 
   useEffect(() => {
     const tareasPendientes = listaTareas.filter(t => t.estado === 'Pendiente').length;
     const tareasCompletadas = listaTareas.filter(t => t.estado === 'Completada').length;
 
-    setPendientes(tareasPendientes);
-    setCompletadas(tareasCompletadas);
-    setResumen("Pendientes (" + pendientes + " / Completadas (" + completadas + ")");
+    setResumen("Pendientes (" + tareasPendientes + " / Completadas (" + tareasCompletadas + ")");
   }, [listaTareas]);
   return (
     <>
